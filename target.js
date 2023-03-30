2a163f70c72d785d19437a8cd2ed07a7de649ddf
Maia's Programs <joaomiguelmaia@netcabo.pt>
2023-03-30 22:21:34 +0100

    Organizado e colorido

--- target.js
+++ target.js
@@ -22,69 +22,76 @@ class Target
   // and its label
 
 
-chose_color() {
-   // Define a color for each group
-   const red = color(255, 0, 0);
-   const yellow = color(255,255,0);
-   const orange = color(255,127,0);
-   const green = color(85, 178, 78);
-   const darkGreen = color(0,90,0);
-   const smoothie = color(255,136,171);
-   const white = color(255,255,255);
-   const colorJuices = color(115,157,250);
-   const colorCream = color(231,141,130);
-   const colorPotato = color(183, 146, 104);
-   const colorPepper = color(187, 17, 42);
-   const colorTomato = color(216, 50, 0);
-   const berinjela = color(105,59,88);
-   const kiwiColor = color(122,150,15);
-   const mangoColor = color(255,205,72);
-   const papayaColor = color(255,194,129);
-   const purple = color(134,70,101);
-   const peachColor = color(239,113,70);
-   const pearColor = color(209,226,49);
-   const yoghurtColor = color(225, 175, 183);
-   const lightGreen = color(115,215,70);
-   const garlicColor = color(242,244,210);
-   const mushroomColor = color(216,204,192);
-   const redBeetColor = color(122,31,61);
+    chose_color() {
+        // Define a color for each group
+        const red = color(255, 0, 0);
+        const yellow = color(255,255,0);
+        const orange = color(255,127,0);
+        const green = color(85, 178, 78);
+        const darkGreen = color(0,90,0);
+        const smoothie = color(255,136,171);
+        const white = color(255,255,255);
+        const colorJuices = color(115,157,250);
+        const colorCream = color(231,141,130);
+        const colorPotato = color(183, 146, 104);
+        const colorPepper = color(187, 17, 42);
+        const colorTomato = color(216, 50, 0);
+        const berinjela = color(105,59,88);
+        const kiwiColor = color(122,150,15);
+        const mangoColor = color(255,205,72);
+        const papayaColor = color(255,194,129);
+        const purple = color(134,70,101);
+        const peachColor = color(239,113,70);
+        const pearColor = color(209,226,49);
+        const yoghurtColor = color(225, 175, 183);
+        const lightGreen = color(0,180,0);
+        const garlicColor = color(242,244,210);
+        const mushroomColor = color(216,204,192);
+        const redBeetColor = color(122,31,61);
+        const pink = color(255,0,255);
+        const gold = color(255,215,0)
 
-   const index = listas.findIndex(item => item[1] === this.id);
+        const index = listas.findIndex(item => item[1] === this.id);
 
-   if ((index >= 0 && index <= 4) || index == 22) return red;
-   if (index ==  7 || index ==  11|| index ==  12 ||index == 19 || index == 78) return yellow;
-   if (index == 10) return kiwiColor;
-   if (index == 13) return mangoColor;
-   if (index >= 24 && index <= 27 || index == 79 || index == 72) return darkGreen;
-   if ((index >= 14 && index <= 15) || index == 23 || index == 71) return orange;
-   if (index == 16) return papayaColor;
-   if (index == 17 || (index >= 20 && index <= 21)) return purple;
-   if (index == 18) return peachColor;
-   if (index >= 8 && index <= 9 || index == 5) return pearColor;
-   if (index >= 28 && index <= 35) return colorJuices;
-   if (index == 36) return smoothie;
-   if (index >= 37 && index <= 46) return white;
-   if (index >= 47 && index <= 55) return yoghurtColor;
-   if (index >= 56 && index <= 57) return colorCream;
-   if (index == 70 || index == 68 || index == 6 || index == 75) return lightGreen;
-   if (index == 69) return berinjela;
-   if (index == 73) return garlicColor;
-   if (index == 74 || (index >= 58 && index <= 60)) return colorPotato;
-   if (index == 76) return mushroomColor;
-   if (index >= 61 && index <= 64) return colorPepper;
-   if (index == 77) return redBeetColor;
-   if (index >= 65 && index <= 67) return colorTomato;
-   if (index == 79) return darkGreen;
+        if ([7,23,24,25,60,71,72,75,76].includes(index)) return red;
+        if ([2,10,11,19,78].includes(index)) return yellow;
+        if ([14,18,15,26,63].includes(index)) return orange;
+        if ([28,29,30,31,32,33,34,35,36].includes(index)) return colorJuices;
+        if ([36].includes(index)) return smoothie;
+        if ([37,38,39,40,41,42,43,44,45,46,77].includes(index)) return white;
+        if ([47,48,49,50,51,52,53,54,55].includes(index)) return yoghurtColor;
+        if ([56,57].includes(index)) return colorCream;
+        if ([59].includes(index)) return berinjela;
+        if ([65].includes(index)) return garlicColor;
+        if ([69,74].includes(index)) return colorPotato;
+        if ([61,68,70,73].includes(index)) return colorPepper;
+        if ([0,1,2,3,3,4,8,9,12,16,17,21,22,64,79,62,67,58,66].includes(index)) return darkGreen;
+        if ([20].includes(index)) return pink;
+        if ([6].includes(index)) return gold;
+        if ([5,13,27].includes(index)) return lightGreen;
+        //if ([].includes(index)) return kiwiColor;
+        //if ([].includes(index)) return mangoColor;
+        //if ([].includes(index)) return papayaColor;
+        //if ([].includes(index)) return peachColor;
+        //if ([].includes(index)) return pearColor;
+        //if ([].includes(index)) return redBeetColor;
+        //if ([].includes(index)) return colorTomato;
+        //if ([].includes(index)) return mushroomColor;
+        //if ([].includes(index)) return purple;
 
-   // Default color if not found in any group
-   return color(255, 255, 255);
- }
+        // Default color if not found in any group
+        return color(100, 100, 100);
+    }
 
   label_color(){
       const index = listas.findIndex(item => item[1] === this.id);
-      const pretos = [5,6,7,8,9,11,12,13,16,19,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,68,70,73,75,76,78];
-      if(pretos.includes(index)){return color(0,0,0)}
-      else return color(255,255,255);
+
+      if(
+          [2,10,11,19,78,37,38,39,40,41,42,43,44,45,46,77,47,48,49,50,51,52,53,54,55,56,57,65,6,69,74]
+              .includes(index)
+        )return color(0,0,0);
+
+      return color(255,255,255);
   }
 
 
